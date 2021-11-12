5ae6fb444d38631fa773a52229615e9fdd5b619b
Manthan Khandale <manthankhandale@gmail.com>
2021-11-12 23:09:40 +0530

    patient repository added

---
+++ lib/patient/repository/patient_repository.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:my_reports/patient/model/patient.dart';
+
+class PatientRepository {
+  Dio dio = Dio();
+
+  Future<Patient?> getPatientDetails(String id) async {
+    final response = await dio.get('');
+
+    if (response.statusCode == 200) {
+      return Patient.fromMap(response.data);
+    }
+    return null;
+  }
+
+  Future<bool> updatePatientDetails(Patient patient) async {
+    final response = await dio.put('', data: patient.toMap());
+
+    if (response.statusCode == 200) {
+      return true;
+    }
+
+    return false;
+  }
+}
